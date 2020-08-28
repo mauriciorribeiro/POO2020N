d5235d6d25cadb8ece8f84fbed0af517e5507d43
Mauricio Ribeiro <mauricio.ribeiro@outlook.com.br>
2020-08-28 11:25:22 -0300

    Adicionada Questao 3

---
+++ S02/funcoes/3.fibonacci/README.md
@@ -0,0 +1,10 @@
+# POO2020N
+Programaçao Orientada a Objetos
+
+Semana 02
+
+Código da função que retorna o n-ésimo termo da séria de Fibonacci.
+
+**** Em implementação ***
+
+O executável "fibonacci" compatível com linux x64.
---
+++ S02/funcoes/3.fibonacci/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+
+
+    return 11;
+    }
+
+int main (void){
+    int n;
+    
+
+
+    cout << "Digite o termo da séria de Fibonacci: ";
+    cin >> n;
+
+    cout << "O termo da séria de Fibonacci F(" << n << ") = " << fibonacci(n) << endl;
+
+    return 0;
+    
+}
