92ca800e1e25cd0bf2a0628900a72653edfe53a8
Mauricio Ribeiro <mauricio.ribeiro@outlook.com.br>
2020-08-30 16:10:24 -0300

    Adicionada Questao 6

--- S02/funcoes/6.pi/README.md
+++ S02/funcoes/6.pi/README.md
@@ -5,6 +5,5 @@ Semana 02
 
 Código da função que retorna o valor de pi, de acordo com a fórmula de Leibniz.
 
-**** Em implementação ***
 
 O executável "pi" compatível com linux x64.
--- S02/funcoes/6.pi/pi.cpp
+++ S02/funcoes/6.pi/pi.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 double pi(int n){
-
-
-    return 11;
+    double parcial = 0;
+    for (int i = 0; i <= n; i++) {
+        parcial += pow(-1,i)/(2.0 * i + 1);
     }
+    
+    return 4*parcial;
+      
+
+}
 
 int main (void){
     int n;
-    
-
 
     cout << "Digite o numero de termos para avaliar o valor de pi com a formula de Leibniz: ";
     cin >> n;
 
-    cout << "O valor de pi é igual a : " << pi(n) << endl;
+    cout << "O valor de pi avaliado com " << n << " termos é igual a : " << pi(n) << endl;
 
     return 0;
     
