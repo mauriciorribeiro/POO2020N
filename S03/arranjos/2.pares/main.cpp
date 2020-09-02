9169e231fbc6d2f8d07e5f16a3b828da0402c133
Mauricio Ribeiro <mauricio.ribeiro@outlook.com.br>
2020-09-01 21:32:32 -0300

    Adicionada Questão 2

--- S03/arranjos/2.pares/main.cpp
+++ S03/arranjos/2.pares/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     int n = 10;     // Quantidade de ítens no vetor
-    int vet[n] = {1,2,3,4,5,6,7,8,9,10};
+    int vet[n] = {1,2,3,4,5,6,7,8,10,12};
 
     cout << "A quantidade de números pares é: " << pares(vet,n) << endl;
 
