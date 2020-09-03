665aa061595ac2749e712ad079b610ab98d48c6c
Mauricio Ribeiro <mauricio.ribeiro@outlook.com.br>
2020-09-02 22:40:25 -0300

    Adicionada Questão 4

---
+++ S03/arranjos/4.avalia/README.md
@@ -0,0 +1,26 @@
+# POO2020N
+
+### Programaçao Orientada a Objetos
+
+#### Questão 3
+
+Função que recebe como parâmetro um vetor de números inteiros *vet* de tamanho *n* definido e inverte a ordem.
+
+Usa o comando *`make`* para compilar executáveis para Linux, Windows x64 e WIndows x32. Testado em sistema Linux x64, com g++, MinGW cross compiler, e demais dependências resolvidas.
+
+Compilar usando:
+
+- Para compilar para Linux
+  
+  > `make all`
+- Para compilar para Windows x64
+  
+  > `make w64`
+- Para compilar para Windows x32
+  
+  > `make w32`
+
+*Executável **inverte"** compatível com Linux x64.
+Executável **inverte_x64.exe** compatível com Windows 64.
+Executável **inverte_x32.exe** compatível com WIndows 32.*
+
---
+++ S03/arranjos/4.avalia/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip> 
+#include <cmath>
+
+#include "avalia.h"
+
+using namespace std;
+
+int main() {
+    int grau = 3;
+    double polinomio[grau] = {12,2,-3,1.4};
+    double x = 6.0;
+
+    cout << "Avaliacao do polinomio:" << endl;
+    cout << "f(" << fixed << setprecision(1) << x << ") = " << fixed;
+    for(int i =grau; i>=0; i--){
+        cout << " ";
+        if (polinomio[i]>0) 
+            cout << "+" << polinomio[i];
+        else if (polinomio[i]<0)
+            cout << "-" << abs(polinomio[i]);
+        if (i>1)
+            cout << "x" << i;
+        else if (i==1)
+            cout << "x";
+    }
+    cout << " = " << avalia(polinomio,grau,x) << endl;
+
+
+    return 0;
+}
+
+
+
