efe5fb9323de2e32eb0ee105e91b85db7fe57bfa
Mauricio Ribeiro <mauricio.ribeiro@outlook.com.br>
2020-09-12 09:49:21 -0300

    Adicionada Questão Feita pelo Arthur Burlet

--- S04/extra/ordena_ArthurBurlet.cpp
+++ S04/extra/ordena_ArthurBurlet.cpp
@@ -1,17 +1,18 @@
+//Código feito por Arthur Fernandes de Burlet
+
 #include <iostream>
 using namespace std; 
-
   
-/*void swap(int *x, int *y)  
+void swap(int *x, int *y)  
 {  
     int temp = *x;  
     *x = *y;  
     *y = temp;  
-}*/  
+}  
   
 void selectionSort(int vet[], int n)  
 {  
-    int i, j, menor, temp;  
+    int i, j, menor, aux;  
   
     for (i = 0; i < n-1; i++)  
     {  
@@ -20,29 +21,21 @@ void selectionSort(int vet[], int n)
         if (vet[j] < vet[menor])  
             menor = j;  
   
-
-    for (int i = 0; i<(int)(n/2); i++){
-        temp = vet[i];
-        vet[i] = vet[n-i-1];
-        vet[n-i-1] = temp;
-    }
-
-}
-        //inverte(vet[menor], vet[i]);  
-        temp = vet[menor];
+        //swap(vet[menor], vet[i]);
+        aux = vet[menor];
         vet[menor] = vet[i];
-        vet[i] = temp;
-    }  
+        vet[i] = aux;
 
-int main()  
+    }  
+}  
+
+int main(void)  
 {  
     int vet[] = {22, 2, 44, 4, 5};  
     int n = sizeof(vet)/sizeof(vet[0]);  
     selectionSort(vet, n);  
-
-
     cout <<"\n";  
-
+    
      for(int i=0;i<n; i++){
 
       cout<<vet[i] <<" ";
