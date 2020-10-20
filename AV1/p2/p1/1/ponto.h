2d4305b59636d9184c29c631ef98151e74c4e3d2
Mauricio Ribeiro <mauricio.ribeiro@outlook.com.br>
2020-10-20 19:58:36 -0300

    AV1

--- AV1/p2/p1/1/ponto.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct ponto;
-ponto* ponto_aloca(float x, float y);
-void ponto_libera(ponto * p);
-void ponto_imprime(ponto * p);
-
-//parte 2
-float ponto_distancia(ponto* p1, ponto* p2);
